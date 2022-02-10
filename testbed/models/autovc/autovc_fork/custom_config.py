f174bd3cafb703a09bc351feb47fa52cfa7c99d9
Tim <timothyfdavison@gmail.com>
2022-02-09 19:29:56 -0500

    Extending AutoVC

---
+++ testbed/models/autovc/autovc_fork/custom_config.py
@@ -0,0 +1,6 @@
+# Configuration file for AutoVC model #
+
+# Model checkpoints
+autovc_checkpoint = "../../../checkpoints/autovc.ckpt"
+speaker_encoder_checkpoint = "../../../checkpoints/autovc_encoder.ckpt"
+wavenet_checkpoint = None
